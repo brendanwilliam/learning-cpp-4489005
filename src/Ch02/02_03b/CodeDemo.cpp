d100fb68d7dde2756c7c9a2902796675074288b6
Brendan Keane <85752216+brendanwilliam@users.noreply.github.com>
2024-11-07 23:41:05 +0000

    Finished exercise 02_03

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -1,12 +1,20 @@
-// Learning C++ 
+// Learning C++
 // Exercise 02_03
-// Using Variables, by Eduardo Corpeño 
+// Using Variables, by Eduardo Corpeño
 
 #include <iostream>
 
+int a, b = 5;
+
 int main(){
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+
     std::cout << "Hi There!" << std::endl;
-    
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
