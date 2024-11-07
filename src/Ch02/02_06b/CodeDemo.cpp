22bb445a41fab54e491a0be94a2c4c424796fd69
Brendan Keane <85752216+brendanwilliam@users.noreply.github.com>
2024-11-07 23:58:53 +0000

    Finished exercise 02_06

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,24 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+
+/* DIRECTIVES */
+#define CAPACITY 5000
+#define DEBUG
+
 
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+#ifdef DEBUG
+    std::cout << "The large integer is " << large << std::endl;
+    std::cout << "The small integer is " << small << std::endl;
+#endif
+    large += small;
+    std::cout << "The large integer is " << large << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
