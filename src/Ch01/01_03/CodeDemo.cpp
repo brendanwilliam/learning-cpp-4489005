d25e6978f5505bbe6b7899757c25172d95043774
Brendan Keane <85752216+brendanwilliam@users.noreply.github.com>
2024-11-07 23:14:14 +0000

    Completed exercise 01_03

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,7 +7,9 @@
 
 int main(){
     std::string str;
+    std::cout << "Enter a string: " << std::flush;
     std::cin >> str;
+    std::cout << "Nice to meet you, " << str << "!" << std::endl;
     std::cout << str;
 
     std::cout << std::endl << std::endl;
