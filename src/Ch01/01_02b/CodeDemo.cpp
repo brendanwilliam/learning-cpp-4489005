2fdc3fae3e7a468f80f430c6c798181d27ee917f
Brendan Keane <85752216+brendanwilliam@users.noreply.github.com>
2024-11-07 23:05:15 +0000

    Finished Ch 01_02

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
-// Learning C++ 
+// Learning C++
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by Eduardo Corpeño
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello, World!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return(0);
+}
